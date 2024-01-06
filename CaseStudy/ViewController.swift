2e29688d404917d8f07b576ca1fe6ae2035ab137
TURKCELL\TCABUYUKHATIPOGLU <arda.buyukhatipoglu@turkcell.com.tr>
2024-01-06 10:12:36 +0300

    Initial Commit

---
+++ CaseStudy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CaseStudy
+//
+//  Created by ARDA BUYUKHATIPOGLU on 6.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
