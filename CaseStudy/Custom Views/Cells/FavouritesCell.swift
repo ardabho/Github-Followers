cc6adf4d47e3c823adad938a70005304f14a4176
TURKCELL\TCABUYUKHATIPOGLU <arda.buyukhatipoglu@turkcell.com.tr>
2024-01-13 20:10:19 +0300

    Add Favourites tab

---
+++ CaseStudy/Custom Views/Cells/FavouritesCell.swift
@@ -0,0 +1,23 @@
+//
+//  FavouritesCell.swift
+//  CaseStudy
+//
+//  Created by ARDA BUYUKHATIPOGLU on 13.01.2024.
+//
+
+import UIKit
+
+class FavouritesCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
